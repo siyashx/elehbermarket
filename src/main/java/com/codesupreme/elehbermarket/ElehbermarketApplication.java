8b19fce39443be239e2647c11cbedb7d21d58342
siyashx <siyashxyusifov@gmail.com>
2025-07-19 03:06:58 +0400

    Initial commit

---
+++ src/main/java/com/codesupreme/elehbermarket/ElehbermarketApplication.java
@@ -0,0 +1,13 @@
+package com.codesupreme.elehbermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElehbermarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElehbermarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codesupreme/elehbermarket/ElehbermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesupreme.elehbermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElehbermarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
